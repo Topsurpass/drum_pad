fabdcd5664d39ea45db576713403a950e00cca19
Olowosuyi Temitope <temitopeabiodun685@gmail.com>
2023-09-07 22:48:03 +0100

    trying on 0.0.0.0 IP

--- src/Landing_page_Compo/landing_page.js
+++ src/Landing_page_Compo/landing_page.js
@@ -34,7 +34,7 @@ export const LandingPage = () => {
             'email' : email,
             'message' : messg
         };
-        axios.post('https://localhost:5000/api/v1/', data, {
+        axios.post('http://0.0.0.0:5000/api/v1/', data, {
             headers: {
                 'Content-Type': 'application/json',
             },
